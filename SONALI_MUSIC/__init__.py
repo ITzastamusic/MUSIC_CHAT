543ca3c24e4a03af8b6f15e080af488e2fa24547
ITzastamusic <astamongo@gmail.com>
2025-12-04 16:34:25 +0530

    Add files via upload

---
+++ SONALI_MUSIC/__init__.py
@@ -0,0 +1,44 @@
+# =======================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (suraj08832) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @brahix
+# =======================================================
+
+from SONALI_MUSIC.core.bot import Sona
+from SONALI_MUSIC.core.dir import dirr
+from SONALI_MUSIC.core.git import git
+from SONALI_MUSIC.core.userbot import Userbot
+from SONALI_MUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Sona()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (suraj08832) 😎
+
+# 🧑‍💻 Developer : t.me/brahix
+# 🔗 Source link : GitHub.com/suraj08832/Sonali-MusicV2
+# 📢 Telegram channel : t.me/about_brahix
+# =======================================================
