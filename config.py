5abff862ccd1b050c1278c5ba2e3ed4cdc134e4d
ITzastamusic <astamongo@gmail.com>
2025-12-07 06:04:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/suraj08832/Sonali-MusicV2")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ITzastamusic/MUSIC_CHAT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", "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P")
