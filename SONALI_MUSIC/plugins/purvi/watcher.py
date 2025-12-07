1d5c4c6fa7be1889cf9dfd853999543d2f65e1d1
ITzastamusic <astamongo@gmail.com>
2025-12-07 05:44:21 +0530

    Add files via upload

---
+++ SONALI_MUSIC/plugins/purvi/watcher.py
@@ -0,0 +1,30 @@
+# =======================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (suraj08832) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @brahix
+# =======================================================
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from SONALI_MUSIC import app
+from SONALI_MUSIC.core.call import Sona
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Sona.stop_stream_force(message.chat.id)
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (suraj08832) 😎
+
+# 🧑‍💻 Developer : t.me/brahix
+# 🔗 Source link : GitHub.com/suraj08832/Sonali-MusicV2
+# 📢 Telegram channel : t.me/about_brahix
+# =======================================================
