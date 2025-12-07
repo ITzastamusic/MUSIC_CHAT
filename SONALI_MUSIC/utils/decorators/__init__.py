9f2eb22271336192239d103412931835b94354b4
ITzastamusic <astamongo@gmail.com>
2025-12-07 05:36:59 +0530

    Add files via upload

---
+++ SONALI_MUSIC/utils/decorators/__init__.py
@@ -0,0 +1,18 @@
+# =======================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (suraj08832) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @brahix
+# =======================================================
+
+from .admins import *
+from .language import *
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (suraj08832) 😎
+
+# 🧑‍💻 Developer : t.me/brahix
+# 🔗 Source link : GitHub.com/suraj08832/Sonali-MusicV2
+# 📢 Telegram channel : t.me/about_brahix
+# =======================================================
